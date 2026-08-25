25aee2af99f89c9a740ea08674e6919cbff8d085
Tati <jaelgonzales18@gmail.com>
2026-08-25 18:27:08 -0400

    clase dieciseis

---
+++ clase16/minesweeper.js
@@ -0,0 +1,25 @@
+"use strict";
+// console.log("This is a minesweeper")
+// let table = document.querySelector<HTMLTableElement>(".minesweeper")
+// if(table) {
+//     console.log(table.tagName)
+//     console.log(table.classList)
+// }
+// let cell = document.querySelector<HTMLTableCellElement>("tr")
+// if(cell) {
+//     console.log(cell.textContent)
+// }
+let elementList = document.querySelectorAll("td");
+for (const element of elementList) {
+    element.addEventListener('click', function () {
+        console.log(element.textContent);
+        console.log(element.classList.value);
+        if (element.classList.contains("bomb")) {
+            for (const item of elementList) {
+                item.textContent = "💥";
+                item.style.color = "black";
+            }
+        }
+        element.style.color = "black";
+    });
+}
