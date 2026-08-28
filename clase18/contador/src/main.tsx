fc06bfadc11ae2ae0512d4d8f2a96407c6e211a2
Tati <jaelgonzales18@gmail.com>
2026-08-28 18:06:57 -0400

    clase 18

---
+++ clase18/contador/src/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import EjemploLlaves from './EjemploLlaves.tsx'
+import Contador from './Contador.tsx'
+import { StrictMode } from 'react'
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <EjemploLlaves/>,
+      <Contador/>
+    </StrictMode>
+)
